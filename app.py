bcca1a90a1dfb834661bf8f1dd45edc17cc7da1f
ivanEstudios <ivanestudios2004@gmail.com>
2024-12-11 09:58:25 +0100

    primeros añadidos

---
+++ app.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+from flask import Flask, render_template
+from variables import cargarVariables
+
+app = Flask(__name__)
+
+app.config.from_pyfile('settings.py')
+#cargarVariables()
+
+@app.route("/")
+def inicio():
+    return render_template("index.html")
+    
+#import rutas_inicio
+
+#import rutas_juegos
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT'))
+    host = os.environ.get('HOST')
+    app.run(host=host, port=port)
