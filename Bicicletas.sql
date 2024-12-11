1dbe95228d2ea22dcb4e4d4af212cb9721a7bb50
DarkShinigami69 <alberto.sanz22@educa.madrid.org>
2024-12-11 11:08:13 +0100

    Creación tabla1

--- Bicicletas.sql
+++ Bicicletas.sql
@@ -1,1 +1,9 @@
-erpifhjrewopihjweopijf
+CREATE DATABASE IF NOT EXISTS DEATHBIKES;
+USE DEATHBIKES;
+CREATE TABLE IF NOT EXISTS bicicletas{
+    id int auto_increment unsigned not null primary key,
+    precio decimal (8,2) not null,
+    color varchar (255) not null,
+    modelo varchar (255) not null, 
+    tipo varchar (255) not null, 
+};
