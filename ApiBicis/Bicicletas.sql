b1a9bc002a17de041bff0737ca65749d7c2c66f6
alejandroo-03 <alejandro.mingo3@educa.madrid.org>
2025-01-15 09:51:29 +0100

    Base de datos

--- ApiBicis/Bicicletas.sql
+++ ApiBicis/Bicicletas.sql
@@ -1,28 +1,30 @@
 CREATE DATABASE IF NOT EXISTS DEATHBIKES;
 USE DEATHBIKES;
-CREATE TABLE IF NOT EXISTS bicicletas{
-    id int auto_increment unsigned not null primary key,
-    precio decimal (8,2) not null,
-    color varchar (255) not null,
-    modelo varchar (255) not null, 
-    tipo varchar (255) not null, 
-};
-
-CREATE TABLE IF NOT EXISTS usuarios{
-nombre varchar (255) not null,
-clave varchar(18) not null,
-};
-
--- Agregamos inserts
-INSERT INTO bicicletas (precio, color, modelo, tipo) VALUES ('6500', 'Satin Cooper/Smoke', 'Roubaix SL8 Expert', 'Carretera');
-INSERT INTO bicicletas (precio, color, modelo, tipo) VALUES ('7500', 'Blanco Mate', 'Venge Pro', 'Carretera');
-INSERT INTO bicicletas (precio, color, modelo, tipo) VALUES ('8200', 'Rojo Brillante', 'Tarmac SL7', 'Carretera');
-INSERT INTO bicicletas (precio, color, modelo, tipo) VALUES ('9200', 'Negro Carbono', 'Madone SLR 9', 'Carretera');
-INSERT INTO bicicletas (precio, color, modelo, tipo) VALUES ('6800', 'Azul Marino', 'Canyon Aeroad CF SLX', 'Carretera');
 
 
-INSERT INTO usuarios (nombre,clave)VALUES("Alejandro","1234");
-INSERT INTO usuarios (nombre,clave)VALUES("Ivan","1234");
-INSERT INTO usuarios (nombre,clave)VALUES("Fernando","Clave$1");
-INSERT INTO usuarios (nombre,clave)VALUES("Alberto","1234");
-INSERT INTO usuarios (nombre,clave)VALUES("Invitado","1234");
+CREATE TABLE IF NOT EXISTS bicicletas(    
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,    
+    precio DECIMAL(8,2) NOT NULL,    
+    color VARCHAR(255) NOT NULL,    
+    modelo VARCHAR(255) NOT NULL,    
+    tipo VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS usuarios(
+    nombre VARCHAR(255) NOT NULL,
+    clave VARCHAR(255) NOT NULL
+);
+
+INSERT INTO bicicletas (precio, color, modelo, tipo) VALUES (6500, 'Satin Cooper/Smoke', 'Roubaix SL8 Expert', 'Carretera');
+INSERT INTO bicicletas (precio, color, modelo, tipo) VALUES (7500, 'Blanco Mate', 'Venge Pro', 'Carretera');
+INSERT INTO bicicletas (precio, color, modelo, tipo) VALUES (8200, 'Rojo Brillante', 'Tarmac SL7', 'Carretera');
+INSERT INTO bicicletas (precio, color, modelo, tipo) VALUES (9200, 'Negro Carbono', 'Madone SLR 9', 'Carretera');
+INSERT INTO bicicletas (precio, color, modelo, tipo) VALUES (6800, 'Azul Marino', 'Canyon Aeroad CF SLX', 'Carretera');
+
+
+INSERT INTO usuarios (nombre, clave) VALUES ('Alejandro', '1234');
+INSERT INTO usuarios (nombre, clave) VALUES ('Ivan', '1234');
+INSERT INTO usuarios (nombre, clave) VALUES ('Fernando', 'Clave$1');
+INSERT INTO usuarios (nombre, clave) VALUES ('Alberto', '1234');
+INSERT INTO usuarios (nombre, clave) VALUES ('Invitado', '1234');
