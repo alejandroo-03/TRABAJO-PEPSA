d0398f66620f16b2797a8fbd160f088e4da90b5d
ivanEstudios <ivanestudios2004@gmail.com>
2025-01-22 10:18:58 +0100

    crearn Test

---
+++ ApiBicis/web/nuestrasFunciones.py
@@ -0,0 +1,4 @@
+
+
+def CalcularIva(precio):
+    return precio*0.21
---
+++ ApiBicis/web/test.py
@@ -0,0 +1,14 @@
+import unittest
+
+from rutas_usuarios import logout
+import nuestrasFunciones
+
+class TestBicis(unittest.TestCase):
+    
+    def testIva(self):
+        
+        self.assertEqual(21,nuestrasFunciones.CalcularIva(100))
+        
+                       
+if __name__ == "__main__":
+    unittest.main()
