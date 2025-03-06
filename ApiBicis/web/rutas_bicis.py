267628a168d73ab7567af5340276ec56e42b61e1
ivanEstudios <ivanestudios2004@gmail.com>
2025-03-06 11:54:36 +0100

    campos sanitizados

--- ApiBicis/web/rutas_bicis.py
+++ ApiBicis/web/rutas_bicis.py
@@ -2,7 +2,7 @@ import json
 from flask import request, session
 from __main__ import app
 import controlador_bicis as controlador_bicis
-from funciones_auxiliares import Encoder
+from funciones_auxiliares import Encoder, sanitize_input
 
 
 @app.route("/bicis",methods=["GET"])
@@ -12,6 +12,7 @@ def bicis():
 
 @app.route("/bicis/<id>",methods=["GET"])
 def bici_por_id(id):
+    id = sanitize_input(id)
     bici,code = controlador_bicis.obtener_bici_por_id(id)
     return json.dumps(bici, cls = Encoder),code
 
@@ -20,7 +21,12 @@ def guardar_bici():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         bici_json = request.json
-        ret,code=controlador_bicis.insertar_bicis(bici_json["nombre"], bici_json["descripcion"], float(bici_json["precio"]), bici_json["foto"])
+        nombre = sanitize_input(bici_json["nombre"])
+        descripcion = sanitize_input(bici_json["descripcion"])
+        precio = sanitize_input(float(bici_json["precio"]))
+        foto = sanitize_input(bici_json["foto"])
+        
+        ret,code=controlador_bicis.insertar_bicis(nombre, descripcion, precio, foto)
     else:
         ret={"status":"Bad request"}
         code=401
@@ -28,6 +34,7 @@ def guardar_bici():
 
 @app.route("/bicis/<id>", methods=["DELETE"])
 def eliminar_bici(id):
+    id = sanitize_input(id)
     ret,code=controlador_bicis.eliminar_bici(id)
     return json.dumps(ret), code
 
@@ -36,7 +43,12 @@ def actualizar_bici():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
         bici_json = request.json
-        ret,code=controlador_bicis.actualizar_bici(bici_json["id"],bici_json["nombre"], bici_json["descripcion"], float(bici_json["precio"]),bici_json["foto"])
+        id = sanitize_input(bici_json["id"])
+        nombre = sanitize_input(bici_json["nombre"])
+        descripcion = sanitize_input(bici_json["descripcion"])
+        precio = sanitize_input(float(bici_json["precio"]))
+        foto = sanitize_input(bici_json["foto"])
+        ret,code=controlador_bicis.actualizar_bici(id,nombre, descripcion, precio,foto)
     else:
         ret={"status":"Bad request"}
         code=401
